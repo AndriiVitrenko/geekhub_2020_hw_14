0c1c114f3bec3a10591ff7aab712727380458045
Andrew Vitrenko <57104862+AndrewVitrenko@users.noreply.github.com>
2021-03-26 04:37:02 +0200

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginPageComponent} from "./components/login-page/login-page.component";
+import {SignupPageComponent} from "./components/signup-page/signup-page.component";
+import {HomeComponent} from "./components/home/home.component";
+import {ArticleListComponent} from "./components/article-list/article-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'log-in',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignupPageComponent,
+  },
+  {
+    path: 'article-list',
+    component: ArticleListComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ArticleListComponent} from './components/article-list/article-list.component';
+import {ArticleComponent} from './components/article/article.component';
+import {LoginPageComponent} from './components/login-page/login-page.component';
+import {SignupPageComponent} from './components/signup-page/signup-page.component';
+
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import { HeaderComponent } from './components/header/header.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import { MainComponent } from './components/main/main.component';
+import {MatCardModule} from "@angular/material/card";
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleListComponent,
+    ArticleComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    HeaderComponent,
+    MainComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatCardModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
